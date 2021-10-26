466f96e6f5d34c290d632b46f62b9722ab166cc3
nbaldzhiev <nenko.baldzhiev@gmail.com>
2021-10-26 16:17:39 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+          <div class="header-bar">
+              <p>PMDB - The Poor Man's Movie Database</p>
+          </div>
+      </header>
+      <main>
+          <div id="search-section">
+              <h1>Welcome.</h1>
+              <h3>Millions of movies, TV shows and people to discover. Explore now.</h3>
+          </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
